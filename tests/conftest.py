60c5c820a2e946e8630a08f73aab12a03a96c305
Anders Gorm Pedersen <agpe@dtu.dk>
2023-01-04 11:39:34 +0100

    Major additions and changes...

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ import random
 def pairfile_string(nodes, pairs, cutoff):
     """Input: list of nodes, list of pair tuples, and cutoff value.
        Output: string that can be written in PAIRFILE, giving each pairwise dist"""
-    pairs = set(pairs)
     stringlist = []
     for n1, n2 in itertools.combinations(nodes,2):
         if ((n1,n2) in pairs) or ((n2,n1) in pairs):
@@ -20,11 +19,18 @@ def pairfile_string(nodes, pairs, cutoff):
 
 @pytest.fixture()
 def random_pairfile_50nodes(tmp_path):
-    nodes = [f"n{i}" for i in range(50)]
-    pairs = []
+    nodes = {f"n{i}" for i in range(50)}
+    pairs = set()
+    n_unpaired = 0
     for n1,n2 in itertools.combinations(nodes,2):
-        if random.random() > 0.2:
-            pairs.append((n1,n2))
+        if random.random() > 0.97:
+            pairs.add((n1,n2))
+        else:
+            n_unpaired += 1
+    if n_unpaired == 0:
+        raise Exception("No unpaired nodes: tests are not covering all aspects")
+    if n_unpaired == 50:
+        raise Exception("All nodes are unpaired: tests are not covering all aspects")
     cutoff = random.random() * 100
     distfile = tmp_path / "distfile.txt"
     distfile.write_text(pairfile_string(nodes, pairs, cutoff))
@@ -33,9 +39,9 @@ def random_pairfile_50nodes(tmp_path):
 ############################################################################################
 
 @pytest.fixture()
-def pairfile_no_neighbors(tmp_path):
-    nodes = [f"n{i}" for i in range(50)]
-    pairs = []
+def random_pairfile_no_neighbors_50nodes(tmp_path):
+    nodes = {f"n{i}" for i in range(50)}
+    pairs = set()
     cutoff = random.random() * 100
     distfile = tmp_path / "distfile.txt"
     distfile.write_text(pairfile_string(nodes, pairs, cutoff))
@@ -43,18 +49,58 @@ def pairfile_no_neighbors(tmp_path):
 
 ############################################################################################
 
+# Graph guaranteed to fail for reduce_from_top
 @pytest.fixture()
-def dist_example_01(tmp_path):
-    nodes = ["n1", "n2", "n3", "n4", "n5", "n6", "n7"]
-    pairs = [("n1", "n2"),
+def graph_example_01(tmp_path):
+    nodes = {"n1", "n2", "n3", "n4", "n5", "n6", "n7"}
+    pairs = {("n1", "n2"),
              ("n1", "n3"),
              ("n1", "n4"),
              ("n2", "n5"),
              ("n3", "n6"),
-             ("n4", "n7")]
+             ("n4", "n7")}
     cutoff = 5
     distfile = tmp_path / "distfile.txt"
     distfile.write_text(pairfile_string(nodes, pairs, cutoff))
     return distfile, nodes, pairs, cutoff
+
+############################################################################################
+
+# Graph guaranteed to fail for reduce_from_bottom
+@pytest.fixture()
+def graph_example_02(tmp_path):
+    nodes = {"n1", "n2", "n3", "n4", "n5", "n6", "n7"}
+    pairs = {("n1", "n2"), ("n1", "n3"), ("n1", "n4"),
+             ("n2", "n5"), ("n2", "n6"), ("n2", "n7"),
+             ("n3", "n5"), ("n3", "n6"), ("n3", "n7"),
+             ("n4", "n5"), ("n4", "n6"), ("n4", "n7"),
+             ("n5", "n6"), ("n5", "n7"),
+             ("n6", "n7")}
+    cutoff = 5
+    distfile = tmp_path / "distfile.txt"
+    distfile.write_text(pairfile_string(nodes, pairs, cutoff))
+    return distfile, nodes, pairs, cutoff
+
+############################################################################################
+
+@pytest.fixture()
+def keepfile_n0_to_n9(tmp_path):
+    keepfile = tmp_path / "keepfile.txt"
+    keepset = {f"n{i}" for i in range(10)}
+    with open(keepfile, "w") as outfile:
+        for nodename in keepset:
+            outfile.write(nodename + "\n")
+    return keepfile, keepset
+
+############################################################################################
+
+@pytest.fixture()
+def keepfile_n3_and_n5(tmp_path):
+    keepfile = tmp_path / "keepfile.txt"
+    keepset = {"n3", "n5"}
+    with open(keepfile, "w") as outfile:
+        for nodename in keepset:
+            outfile.write(nodename + "\n")
+    return keepfile, keepset
 
 
--- tests/test_greedyreduce.py
+++ tests/test_greedyreduce.py
@@ -2,16 +2,19 @@ import pytest
 import greedyreduce
 import itertools
 import collections
+import copy
 
 
 ###################################################################################################
 ###################################################################################################
 
 class Args:
-    def __init__(self, infile, values_are_sim=False, cutoff=5):
+    """Used to mock args object normally returned from argparser"""
+    def __init__(self, infile, values_are_sim=False, cutoff=5, keepfile=None):
         self.cutoff = cutoff
         self.infile = infile
         self.values_are_sim = values_are_sim
+        self.keepfile = keepfile
 
 ###################################################################################################
 ###################################################################################################
@@ -28,8 +31,8 @@ class Test_init:
                 assert n2 in gr.neighbors[n1]
                 assert n1 in gr.neighbors[n2]
             else:
-                assert n2 not in gr.neighbors[n1]
-                assert n1 not in gr.neighbors[n2]
+                assert (n1 not in gr.neighbors) or (n2 not in gr.neighbors[n1])
+                assert (n2 not in gr.neighbors) or (n1 not in gr.neighbors[n2])
         nbcountlist = []
         for node,count in gr.neighbor_count.items():
             c = 0
@@ -38,26 +41,32 @@ class Test_init:
                     c += 1
             nbcountlist.append(c)
             assert c == count
-        assert gr.orignum == len(nodes)
-        assert gr.average_degree == sum(nbcountlist) / len(nodes)
-        assert gr.max_degree == max(nbcountlist)
-        assert gr.min_degree == min(nbcountlist)
+        assert gr.origdata["orignum"] == len(nodes)
+        assert gr.origdata["average_degree"]  == sum(nbcountlist) / len(nodes)
+        assert gr.origdata["max_degree"] == max(nbcountlist)
+        assert gr.origdata["min_degree"] == min(nbcountlist)
         # how to check gr.average_dist from given info???
 
-    def test_example_without_neighbors(self, pairfile_no_neighbors):
-        distfile, nodes, pairs, cutoff = pairfile_no_neighbors
+    def test_example_without_neighbors(self, random_pairfile_no_neighbors_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_no_neighbors_50nodes
         args = Args(infile=distfile, cutoff=cutoff)
         gr = greedyreduce.NeighborGraph(args)
         assert gr.nodes == set(nodes)
-        for n1,n2 in itertools.combinations(nodes, 2):
-            assert n2 not in gr.neighbors[n1]
-            assert n1 not in gr.neighbors[n2]
+        for node in nodes:
+            assert node not in gr.neighbors
         for count in gr.neighbor_count.values():
             assert count == 0
-        assert gr.orignum == len(nodes)
-        assert gr.average_degree == 0
-        assert gr.max_degree == 0
-        assert gr.min_degree == 0
+        assert gr.origdata["orignum"] == len(nodes)
+        assert gr.origdata["average_degree"] == 0
+        assert gr.origdata["max_degree"] == 0
+        assert gr.origdata["min_degree"] == 0
+
+    def test_parse_keepset(self, random_pairfile_50nodes, keepfile_n0_to_n9):
+        distfile, nodes, pairs, cutoff = random_pairfile_50nodes
+        keepfile, keepset = keepfile_n0_to_n9
+        args = Args(infile=distfile, cutoff=cutoff, keepfile=keepfile)
+        gr = greedyreduce.NeighborGraph(args)
+        assert gr.keepset == {'n0', 'n1', 'n2', 'n3', 'n4', 'n5', 'n6', 'n7', 'n8', 'n9'}
 
 ###################################################################################################
 ###################################################################################################
@@ -71,10 +80,242 @@ class Test_most_neighbors:
         node_with_most_nb, max_num_nb = gr.most_neighbors()
         pairnodes = [node for tup in pairs for node in tup]
         ncount = collections.Counter(pairnodes)
-        expected_maxnode, expected_maxun = ncount.most_common(1)[0]
-        assert (node_with_most_nb, max_num_nb) == (expected_maxnode, expected_maxun)
+        expected_maxnode, expected_maxnum = ncount.most_common(1)[0]
+        assert max_num_nb == expected_maxnum
+        assert ncount[node_with_most_nb] == max_num_nb # There could be several with same num
 
-    def test_example_without_neighbors(self, pairfile_no_neighbors):
-        distfile, nodes, pairs, cutoff = pairfile_no_neighbors
+    def test_example_without_neighbors(self, random_pairfile_no_neighbors_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_no_neighbors_50nodes
         args = Args(infile=distfile, cutoff=cutoff)
         gr = greedyreduce.NeighborGraph(args)
+        node_with_most_nb, max_num_nb = gr.most_neighbors()
+        assert node_with_most_nb is None
+        assert max_num_nb == 0
+
+###################################################################################################
+###################################################################################################
+
+class Test_fewest_neighbors:
+
+    def test_example_with_neighbors(self, random_pairfile_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_50nodes
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        node_with_fewest_nb, min_num_nb = gr.fewest_neighbors()
+        pairnodes = [node for tup in pairs for node in tup]
+        ncount = collections.Counter(pairnodes)
+        expected_minnode, expected_minnum = ncount.most_common()[-1]
+        assert min_num_nb == expected_minnum
+        assert ncount[node_with_fewest_nb] == min_num_nb # there could be several with same num
+
+    def test_example_without_neighbors(self, random_pairfile_no_neighbors_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_no_neighbors_50nodes
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        node_with_most_nb, max_num_nb = gr.fewest_neighbors()
+        assert node_with_most_nb is None
+        assert max_num_nb == 0
+
+###################################################################################################
+###################################################################################################
+
+class Test_remove_node:
+
+    def test_example_without_neighbors(self, random_pairfile_no_neighbors_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_no_neighbors_50nodes
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        n1 = nodes.pop()
+        gr.remove_node(n1)
+        assert gr.nodes == set(nodes)
+        assert n1 not in gr.neighbor_count
+        assert n1 not in gr.neighbors
+        for count in gr.neighbor_count.values():
+            assert count == 0
+
+    def test_example_with_neighbors(self, graph_example_01):
+        distfile, nodes, pairs, cutoff = graph_example_01
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        gr.remove_node("n2")
+        nodes.remove("n2")
+        assert gr.nodes == set(nodes)
+        assert "n2" not in gr.neighbor_count
+        assert "n2" not in gr.neighbors
+        expected_counts = {"n1":2, "n3":2, "n4":2, "n6":1, "n7":1}
+        assert gr.neighbor_count == expected_counts
+        expected_neighbors = {"n1":{"n3","n4"}, "n3":{"n1","n6"}, "n4":{"n1","n7"}, "n6":{"n3"}, "n7":{"n4"}}
+        assert gr.neighbors == expected_neighbors
+
+        gr.remove_node("n1")
+        nodes.remove("n1")
+        assert gr.nodes == set(nodes)
+        assert "n1" not in gr.neighbor_count
+        assert "n1" not in gr.neighbors
+        expected_counts = {"n3":1, "n4":1, "n6":1, "n7":1}
+        assert gr.neighbor_count == expected_counts
+        expected_neighbors = {"n3":{"n6"}, "n4":{"n7"}, "n6":{"n3"}, "n7":{"n4"}}
+        assert gr.neighbors == expected_neighbors
+
+###################################################################################################
+###################################################################################################
+
+class Test_remove_connection:
+
+    def test_example_without_neighbors(self, random_pairfile_no_neighbors_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_no_neighbors_50nodes
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        n1,n2 = list(nodes)[:2]
+        with pytest.raises(Exception, match=r"These nodes are not neighbors.*"):
+            gr.remove_connection(n1,n2)
+
+    def test_example_with_neighbors(self, random_pairfile_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_50nodes
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        for n1,n2 in pairs:
+            n1countpre = gr.neighbor_count[n1]
+            n2countpre = gr.neighbor_count[n2]
+            assert n2 in gr.neighbors[n1]
+            assert n1 in gr.neighbors[n2]
+            gr.remove_connection(n1,n2)
+            assert (n1 not in gr.neighbor_count) or (n2 not in gr.neighbors[n1])
+            assert (n2 not in gr.neighbor_count) or (n1 not in gr.neighbors[n2])
+            assert (n1 not in gr.neighbor_count) or (gr.neighbor_count[n1] == n1countpre - 1)
+            assert (n2 not in gr.neighbor_count) or (gr.neighbor_count[n2] == n2countpre - 1)
+
+    def test_example_known_graph(self, graph_example_01):
+        distfile, nodes, pairs, cutoff = graph_example_01
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        gr.remove_connection("n2","n5")
+        gr.remove_connection("n1","n4")
+        assert gr.neighbors["n1"] == {"n2","n3"}
+        assert gr.neighbors["n2"] == {"n1"}
+        assert gr.neighbors["n3"] == {"n1","n6"}
+        assert gr.neighbors["n4"] == {"n7"}
+        assert gr.neighbors["n6"] == {"n3"}
+        assert gr.neighbors["n7"] == {"n4"}
+        assert "n5" not in gr.neighbors
+        with pytest.raises(Exception, match=r"These nodes are not neighbors: n3, n7. Can't remove connection"):
+            gr.remove_connection("n3", "n7")
+
+###################################################################################################
+###################################################################################################
+
+class Test_remove_neighbors:
+
+    def test_example_without_neighbors(self, random_pairfile_no_neighbors_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_no_neighbors_50nodes
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        nbdict_pre = copy.deepcopy(gr.neighbors)
+        for node in nodes:
+            gr.remove_neighbors(node)
+            assert gr.neighbors == nbdict_pre
+
+    def test_example_with_neighbors(self, random_pairfile_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_50nodes
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        nbdict_keys = list(gr.neighbors.keys())  # These nodes have neighbors
+        for node in nbdict_keys:
+            gr.remove_neighbors(node)
+            assert node not in gr.neighbors
+
+    def test_example_known_graph_1(self, graph_example_01):
+        distfile, nodes, pairs, cutoff = graph_example_01
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        gr.remove_neighbors("n1")
+        assert gr.nodes == {"n1", "n5", "n6", "n7"}
+        assert gr.neighbors == {}
+        assert gr.neighbor_count == {}
+
+###################################################################################################
+###################################################################################################
+
+class Test_remove_keepfile_neighbors:
+
+    def test_example_with_neighbors(self, random_pairfile_50nodes, keepfile_n0_to_n9):
+        distfile, nodes, pairs, cutoff = random_pairfile_50nodes
+        keepfile, keepset = keepfile_n0_to_n9
+        args = Args(infile=distfile, cutoff=cutoff, keepfile=keepfile)
+        gr = greedyreduce.NeighborGraph(args)
+        expected_removed = set()
+        for n1,n2 in pairs:
+            if (n1 in keepset) and (n2 not in keepset):
+                expected_removed.add(n2)
+            if (n2 in keepset) and (n1 not in keepset):
+                expected_removed.add(n1)
+        gr.remove_keepfile_neighbors()
+        for node in expected_removed:
+            assert node not in gr.nodes
+            assert node not in gr.neighbors
+            assert node not in gr.neighbor_count
+
+    def test_example_without_neighbors(self,
+                    random_pairfile_no_neighbors_50nodes, keepfile_n0_to_n9):
+        distfile, nodes, pairs, cutoff = random_pairfile_no_neighbors_50nodes
+        keepfile, keepset = keepfile_n0_to_n9
+        args = Args(infile=distfile, cutoff=cutoff, keepfile=keepfile)
+        gr = greedyreduce.NeighborGraph(args)
+        gr.remove_keepfile_neighbors()
+        assert set(nodes) == gr.nodes
+        for node in nodes:
+            assert node not in gr.neighbors
+            assert node not in gr.neighbor_count
+
+    def test_example_known_graph_1(self, graph_example_01, keepfile_n3_and_n5):
+        distfile, nodes, pairs, cutoff = graph_example_01
+        keepfile, keepset = keepfile_n3_and_n5
+        args = Args(infile=distfile, cutoff=cutoff, keepfile=keepfile)
+        gr = greedyreduce.NeighborGraph(args)
+        gr.remove_keepfile_neighbors()
+        assert gr.nodes == {"n3", "n4", "n5", "n7"}
+        assert gr.neighbors["n4"] == {"n7"}
+        assert gr.neighbors["n7"] == {"n4"}
+        assert gr.neighbor_count["n4"] == 1
+        assert gr.neighbor_count["n7"] == 1
+
+###################################################################################################
+###################################################################################################
+
+class Test_reduce_from_top:
+
+    def test_example_known_graph_1(self, graph_example_01):
+        distfile, nodes, pairs, cutoff = graph_example_01
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        gr.reduce_from_top()
+        assert len(gr.nodes) == 3
+        assert ("n2" in gr.nodes) or ("n5" in gr.nodes)
+        assert ("n3" in gr.nodes) or ("n6" in gr.nodes)
+        assert ("n4" in gr.nodes) or ("n7" in gr.nodes)
+        assert gr.neighbors == {}
+        assert gr.neighbor_count == {}
+
+    def test_example_known_graph_2(self, graph_example_02):
+        distfile, nodes, pairs, cutoff = graph_example_02
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        gr.reduce_from_top()
+        assert gr.nodes == {"n2", "n3", "n4"}
+        assert gr.neighbors == {}
+        assert gr.neighbor_count == {}
+
+    def test_random_graph_unknown_result(self, random_pairfile_50nodes):
+        distfile, nodes, pairs, cutoff = random_pairfile_50nodes
+        paired_nodes = {node for tup in pairs for node in tup}
+        unpaired_nodes = nodes - paired_nodes
+        args = Args(infile=distfile, cutoff=cutoff)
+        gr = greedyreduce.NeighborGraph(args)
+        gr.reduce_from_top()
+        assert gr.neighbors == {}
+        assert gr.neighbor_count == {}
+        for node in unpaired_nodes:
+            assert node in gr.nodes
+
+
+
